66412a078e92cabde839f1c33f25343046f5be9a
Shira Biton <shirabiton387@gmail.com>
2025-01-06 13:15:50 +0200

    fix(thankList.tsx): fix edge case in fetching thank list

--- src/app/components/thanks/ThankList.tsx
+++ src/app/components/thanks/ThankList.tsx
@@ -14,7 +14,7 @@ const ThankList = () => {
     // Effect hook to fetch the list of 'thank' data on component mount
     useEffect(() => {
         const fetchThanks = async () => {
-            if (!thankList || thankList.length === 0) {
+            if (!thankList) {
                 const response = await getThanks();
                 if (response)
                     setThankList(response);
@@ -39,7 +39,7 @@ const ThankList = () => {
 
     return (
         <div className="flex justify-between items-center w-full">
-            <button
+            {thankList && thankList.length > 3 && <button
                 onClick={handleLeftClick}
                 className="flex justify-center items-center p-2 md:p-4 bg-transparent hover:bg-gray-200 rounded-full transition-all"
             >
@@ -47,6 +47,7 @@ const ThankList = () => {
                     <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
                 </svg>
             </button>
+            }
 
             <div className="flex gap-6 md:gap-10 justify-center items-center w-full flex-wrap">
                 {thankList && thankList.slice(currentIndex, currentIndex + numOfDisplayedCards)
@@ -58,15 +59,15 @@ const ThankList = () => {
                 }
             </div>
 
-            <button
+            {thankList && thankList.length > 3 && <button
                 onClick={handleRightClick}
                 className="flex justify-center items-center p-2 md:p-4 bg-transparent hover:bg-gray-200 rounded-full transition-all"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 md:w-8 md:h-8 text-primary">
                     <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
                 </svg>
-                
             </button>
+            }
         </div>
     )
 }
