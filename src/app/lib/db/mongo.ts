b9bfb7cada1a0143a058a7e92737af8e7a2b3b45
Shira Biton <shirabiton387@gmail.com>
2025-01-06 13:14:14 +0200

    fix(mongo.ts): fix connecting to mongo

--- src/app/lib/db/mongo.ts
+++ src/app/lib/db/mongo.ts
@@ -9,13 +9,15 @@ const connect = async () => {
     // If there is already connection to MongoDB - prevents another connection
     console.log("Already connected to MongoDB");
   }
-  try {
-  const db = await mongoose.connect(MONGODB_URI);
-  // If readyState is equal to 1, it indicates that the connection is active
-  isConnected = db.connection.readyState === 1;
-  console.log("MongoDB connected successfully");
-  } catch (error) {
-  throw new Error("Error in connecting to MongoDB" + error);
+  else {
+    try {
+      const db = await mongoose.connect(MONGODB_URI);
+      // If readyState is equal to 1, it indicates that the connection is active
+      isConnected = db.connection.readyState === 1;
+      console.log("MongoDB connected successfully");
+    } catch (error) {
+      throw new Error("Error in connecting to MongoDB" + error);
+    }
   }
 };
 
